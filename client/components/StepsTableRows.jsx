05a25ed4c51abd77b46f9f05656ecb21dea797c2
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-07 11:53:23 -0500

    app fully working after decomposing steps

--- client/components/StepsTableRows.jsx
+++ client/components/StepsTableRows.jsx
@@ -1,44 +1,17 @@
 import React from 'react';
+import StepsTableRow from './StepsTableRow';
 
 const StepsTableRows = ({ stepsTracker, setShowModalStep, removeStep }) => {
   const renderBody = () => {
-    return stepsTracker.map(
-      (
-        {
-          id,
-          app_id,
-          date,
-          step_type,
-          contact_name,
-          contact_role,
-          contact_info,
-          notes,
-        },
-        index
-      ) => {
-        return (
-          <tr key={id}>
-            <td>{new Date(date).toLocaleDateString('en-US')}</td>
-            <td>{step_type}</td>
-            <td>{contact_name}</td>
-            <td>{contact_role}</td>
-            <td>{contact_info}</td>
-            <td>{notes}</td>
-            <td className="operation">
-              <button
-                className="deleteButton"
-                onClick={() => setShowModalStep({ action: 'edit', id: index })}
-              >
-                Edit
-              </button>
-              <button className="button" onClick={() => removeStep(app_id, id)}>
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-      }
-    );
+    return stepsTracker.map((stepsTableRow, idx) => (
+      <StepsTableRow
+        key={`stepsTableRow-${idx}`}
+        idx={idx}
+        stepsTableRow={stepsTableRow}
+        setShowModalStep={setShowModalStep}
+        removeStep={removeStep}
+      />
+    ));
   };
   return <tbody>{renderBody()}</tbody>;
 };
