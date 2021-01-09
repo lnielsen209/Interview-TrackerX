21889307c644316459dda0e6240e1e4fbffc55f9
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-08 19:31:00 -0500

    client side auth option 1 completed, save user data in localStorage

--- client/routes/useAuth.jsx
+++ client/routes/useAuth.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, createContext, useState, useEffect } from 'react';
+import React, { useContext, createContext, useState } from 'react';
 
 const authContext = createContext();
 
+// Option 1 save user data in state
 const useProvideAuth = () => {
   const [user, setUser] = useState({
     id: null,
@@ -40,6 +41,43 @@ const useProvideAuth = () => {
   console.log('user in useAuth ===> ', user);
   return { user, login, signup, signout };
 };
+
+// // Option 2 save user data in localStorage
+// const useProvideAuth = () => {
+//   const user = JSON.parse(localStorage.getItem('user')) || {
+//     id: null,
+//     email: null,
+//     isAuthenticated: false,
+//   };
+
+//   const login = (id, email, cb) => {
+//     const user = {
+//       id,
+//       email,
+//       isAuthenticated: true,
+//     };
+//     localStorage.setItem('user', JSON.stringify(user));
+//     cb();
+//   };
+
+//   const signup = (id, email, cb) => {
+//     const user = {
+//       id,
+//       email,
+//       isAuthenticated: true,
+//     };
+//     localStorage.setItem('user', JSON.stringify(user));
+//     cb();
+//   };
+
+//   const signout = (cb) => {
+//     localStorage.removeItem('user');
+//     cb();
+//   };
+
+//   console.log('user in useAuth ===> ', user);
+//   return { user, login, signup, signout };
+// };
 
 export const ProvideAuth = ({ children }) => {
   const auth = useProvideAuth();
