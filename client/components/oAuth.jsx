274a223645ae4b552e34bb4e288cb315ac9a6974
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-12 21:49:55 -0500

    fix files

--- client/components/oAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function oAuth() {
-  const handleClick = (e) => {
-    e.preventDefault();
-  };
-
-  return <button onClick={handleClick}>login with Google</button>;
-}
