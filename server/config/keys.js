178d666c1f4fb6aad2190829ac1633b592096806
AndreaLi <andrea.li8341@hotmail.com>
2021-01-14 12:33:49 -0800

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,14 +1,1 @@
 //add this to .gitignore
-
-module.exports = {
-  google: {
-    clientID:
-      '336634204260-sb2mvllelactkuaen2hepbetmfghekdi.apps.googleusercontent.com',
-    clientSecret: 'LI8nZBUlw-FFNDAcuqBs8hWT',
-  },
-
-  github: {
-    clientID: '28a577b5ddcc653037fb',
-    clientSecret: '92ee6c2caaf715f91f2c24b66fbdc5d0c11bb454',
-  },
-};
