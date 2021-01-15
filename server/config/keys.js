a9410f7d61b19998ab2d5cdae8582235e755d722
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-14 20:23:03 -0500

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,13 +2,12 @@
 
 module.exports = {
   google: {
-    clientID:
-      '336634204260-sb2mvllelactkuaen2hepbetmfghekdi.apps.googleusercontent.com',
-    clientSecret: 'LI8nZBUlw-FFNDAcuqBs8hWT',
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_CLIENTSECRET,
   },
 
   github: {
-    clientID: '28a577b5ddcc653037fb',
-    clientSecret: '92ee6c2caaf715f91f2c24b66fbdc5d0c11bb454',
+    clientID: process.env.GITHUB_CLIENTID,
+    clientSecret: process.env.GITHUB_CLIENTSECRET,
   },
 };
