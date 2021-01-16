8af713541ecef60325c13c66f84d968cf703b465
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-16 09:59:05 -0500

    oAuth buttons styling completed

--- client/components/pages/Signin/oAuth.jsx
+++ client/components/pages/Signin/oAuth.jsx
@@ -1,30 +1,90 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
 import styled from 'styled-components';
 import {
   StyledFormLabel,
   StyledFormInput,
   StyledButton,
   StyledH1,
+  StyledIcon,
   StyledModelForm,
   StyledModelInnerWrapper,
   StyledModelOuterWrapper,
 } from '../../common';
 import { Theme } from '../../../style/Theme';
 
-const OAuthButton = styled(StyledButton)`
+const GoogleButton = styled(StyledButton)`
+  background: ${Theme.secondary};
   width: 50%;
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 500;
+  border: 1px solid rgba(26, 26, 26, 0.3);
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+
+  &:hover {
+    background-color: ${Theme.secondary};
+    box-shadow: inset 0 0 0 1px ${Theme.border};
+    border: 1px solid ${Theme.border};
+  }
+`;
+
+const GithubButton = styled(StyledButton)`
+  background: ${Theme.secondary};
+  width: 50%;
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 500;
+  border: 1px solid rgba(26, 26, 26, 0.3);
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border-left: 0;
+
+  &:hover {
+    background-color: ${Theme.secondary};
+    box-shadow: inset 0 0 0 1px ${Theme.border};
+    border: 1px solid ${Theme.border};
+  }
+`;
+
+const A = styled.a`
+  color: #666666;
+  text-decoration: none;
+
+  &:hover {
+    color: ${Theme.color};
+  }
+`;
+
+const I = styled(StyledIcon)`
+  color: black;
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  margin-right: 6px;
+
+  &:hover {
+    background-color: ${Theme.secondary};
+  }
 `;
 
 const oAuth = () => {
   return (
     <div>
-      <OAuthButton>
-        <a href="/auth/google">Signin with Google</a>
-      </OAuthButton>
-      <OAuthButton>
-        <a href="/auth/github">Signin with Github</a>
-      </OAuthButton>
+      <GoogleButton>
+        <I>
+          <FontAwesomeIcon icon={faGoogle} />
+        </I>
+        <A href="/auth/google">Signin with Google</A>
+      </GoogleButton>
+      <GithubButton>
+        <I>
+          <FontAwesomeIcon icon={faGithub} />
+        </I>
+        <A href="/auth/github">Signin with Github</A>
+      </GithubButton>
     </div>
   );
 };
---
+++ client/style/Theme.js
@@ -0,0 +1,12 @@
+const Theme = {
+  color: '#1a1a1a',
+  color_light: '#464646',
+  background: '#f3f2ef',
+  border: '#E6E6E6',
+  primary: '#0a66c2',
+  primary_hover: '#02478C',
+  secondary: '#ffffff',
+  secondary_hover: '#e8f0fe',
+};
+
+export { Theme };
