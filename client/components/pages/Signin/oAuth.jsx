042df4ff5e8fe562be80090341f6017427d28a38
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-01-16 08:30:07 -0500

    commit

--- client/components/pages/Signin/oAuth.jsx
+++ client/components/pages/Signin/oAuth.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 
-export default function oAuth() {
+import styled from 'styled-components';
+import {
+  StyledFormLabel,
+  StyledFormInput,
+  StyledButton,
+  StyledH1,
+  StyledModelForm,
+  StyledModelInnerWrapper,
+  StyledModelOuterWrapper,
+} from '../../common';
+import { Theme } from '../../../style/Theme';
+
+const OAuthButton = styled(StyledButton)`
+  width: 50%;
+`;
+
+const oAuth = () => {
   return (
     <div>
-      <button>
-        <a
-          // target='_blank'
-          href='/auth/google'
-          style={{ textDecoration: 'none', color: 'white' }}
-        >
-          Signin with Google
-        </a>
-      </button>
-      <button>
-        <a
-          // target='_blank'
-          href='/auth/github'
-          style={{ textDecoration: 'none', color: 'white' }}
-        >
-          Signin with Github
-        </a>
-      </button>
+      <OAuthButton>
+        <a href="/auth/google">Signin with Google</a>
+      </OAuthButton>
+      <OAuthButton>
+        <a href="/auth/github">Signin with Github</a>
+      </OAuthButton>
     </div>
   );
-}
+};
+
+export default oAuth;
